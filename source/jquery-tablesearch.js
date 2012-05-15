cb71141a9320e41658482317a7e8ba5ad74ee35a
Juarez Junior <juareznjunior@gmail.com>
2012-05-15 10:50:55 -0300

    crate TableSearch class, using convencional Class

--- source/jquery-tablesearch.js
+++ source/jquery-tablesearch.js
@@ -1,18 +1,14 @@
 /*!
  * jQuery TableSearch
  * 
- * @autor:.....Juarez Gonçalves Nery Junior
- * @email:.....juareznjunior@gmail.com
- * @twitter:...@juareznjunior
- * 
+ * @autor......: Juarez Gonçalves Nery Junior
+ * @email......: juareznjunior@gmail.com
+ * @twitter....: @juareznjunior
+ * @git........: https://github.com/juareznjunior/jQuery-TableSearch
  */
-;(function($, window, document, undefined){
-	
-	var defaults = {
-		rowIndex: 0
-	}
+;(function($, window, document, undefined) {
 
-	var TableSearch = (function($){
+	var TableSearch = function(elem,selectorTBody,options) {
 
 		// vars
 		// private
@@ -30,71 +26,69 @@
 		// functions
 		// private
 		var _search = function() {
-				var i = 0;
+			var i = 0;
 
-				_selectorTBody.removeClass(_clsShowRows);
+			_selectorTBody.removeClass(_clsShowRows);
 
-				_rows = ( _cache.length > 0 ) ? _cache : _selectorTBody[0].rows;
+			_rows = ( _cache.length > 0 ) ? _cache : _selectorTBody[0].rows;
 
-				while ( _row = _rows[i++] ) {
+			while ( _row = _rows[i++] ) {
+			
+				if ( $(_row.cells[_options.rowIndex]).text().toLowerCase().indexOf($.trim(_elem.value.toLowerCase())) > -1 ) {
 				
-					if ( $(_row.cells[_options.rowIndex]).text().toLowerCase().indexOf($.trim(_elem.value.toLowerCase())) > -1 ) {
+					$(_row)
+						.removeClass(_clsHideRow)
+						.addClass(_clsShowRow);
 					
-						$(_row)
-							.removeClass(_clsHideRow)
-							.addClass(_clsShowRow);
-						
-						if ( $.inArray(_row,_cache) === -1 ) {
-							_cache[_cache.length] = _row;
-						}
-						
-					} else {
-						$(_row)
-							.removeClass(_clsShowRow)
-							.addClass(_clsHideRow);
+					if ( $.inArray(_row,_cache) === -1 ) {
+						_cache[_cache.length] = _row;
 					}
-				}
-			}
-			,_trigger = function() {
-				if ( _elem.value !== '' && _elem.value.length > 3 ) {
-					window.clearTimeout(_timeOut);
-					_timeOut = window.setTimeout(_search,100);
+					
 				} else {
-					_selectorTBody.addClass(_clsShowRows);
-					_cache = [];
-					_rows = null;
+					$(_row)
+						.removeClass(_clsShowRow)
+						.addClass(_clsHideRow);
 				}
 			}
-
-		// expose
-		return {
-			init: function(elem,selectorTBody,options) {
-
-				// input
-				_elem = elem;
-
-				// jQuery Object
-				_selectorTBody = selectorTBody;
-
-				// options
-				_options = options;
-
-				$(_elem).on('keyup',_trigger);
-
+		}
+		,_trigger = function() {
+			if ( _elem.value !== '' && _elem.value.length > 3 ) {
+				window.clearTimeout(_timeOut);
+				_timeOut = window.setTimeout(_search,100);
+			} else {
+				_selectorTBody.addClass(_clsShowRows);
+				_cache = [];
+				_rows = null;
 			}
 		}
 
-	})(jQuery);
+		// input
+		_elem = elem;
+
+		// jQuery Object
+		_selectorTBody = selectorTBody;
+
+		// options
+		_options = $.extend({},{
+			rowIndex: 0
+		},options);
+
+		$(_elem).on('keyup',_trigger);
+
+	};
 	
 	$.fn.tableSearch = function(selectorTBody, options) {
+		return this.each(function(data) {
 
-		return this.each(function(){
-		
 			if ( false === $(this).is(':text') ) {
 				return;
 			}
-			
-			TableSearch.init(this,$(selectorTBody),$.extend(defaults,options));
+
+			data = $.data(this,'TableSearch');
+
+			if ( undefined === data ) {
+				$.data(this,'TableSearch', new TableSearch(this,$(selectorTBody),options));
+			}
 		});
 	}
 	
